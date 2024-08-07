325da62ce1870dedbf339ad2c31c2329c6806955
lxkarp <me@alexkarpinski.com>
2024-08-07 16:32:39 +0000

    feat: successful forecasts and viz for all three segments using Chronos

--- viz_chronos_eval.py
+++ viz_chronos_eval.py
@@ -6,7 +6,7 @@ import numpy as np
 from gluonts.model.evaluation import evaluate_forecasts
 from gluonts.ev.metrics import MASE, MeanWeightedSumQuantileLoss, BaseMetricDefinition, DirectMetric
 from scipy.stats import wasserstein_distance
-from gluonts.ev.aggregations import Mean
+from gluonts.ev.aggregations import Mean, Sum
 
 import os
 import yaml
@@ -69,37 +69,62 @@ def mk_metrics(context, forecast):
                 metrics=[
                     MASE(),
                     MeanWeightedSumQuantileLoss(np.arange(0.1, 1.0, 0.1)),
+                    EMD(),
                 ],
                 batch_size=5000,
             )
-            .reset_index(drop=True)
+            .reset_index(drop=True).rename(
+            {"MASE[0.5]": "MASE", "mean_weighted_sum_quantile_loss": "WQL", "EMD[0.5]":"EMD"},
+            axis="columns",
+        )
             .to_dict(orient="records")
         )
 
-    return metrics
+    return metrics[0] # !!OJO!! Magic numbers to remove the list
 
 def mk_viz(context, forecast, config):
+    metrics = mk_metrics(context, forecast)
+    context = [item for item in context]
+    
     context_by_category = {'casual': context[0][0], 'registered': context[1][0]}
     actuals_by_category = {'casual': context[0][1], 'registered': context[1][1]}
     forecasts_by_category = {'casual': forecast[0], 'registered': forecast[1]}
 
-    # registered only atm
-    context_data_length = len(context_by_category['registered']['target'])
-    context_data_start = context_by_category['registered']['start'].to_timestamp()
+    for cat, cat_data in context_by_category.items():
+        context_data_length = len(cat_data['target'])
+        context_data_start = cat_data['start'].to_timestamp()
 
-    plot_dates = pd.date_range(start=context_data_start, periods=config['prediction_length']+context_data_length, freq='D')
-    fig, ax = plt.subplots()
+        plot_dates = pd.date_range(start=context_data_start, periods=config['prediction_length']+context_data_length, freq='D')
+        fig, ax = plt.subplots()
+    
+        ax.plot(plot_dates, np.append(cat_data['target'], actuals_by_category[cat]['target']))
+        forecasts_by_category[cat].plot(ax=ax, show_label=True)
+        fig.autofmt_xdate()
+        plt.suptitle(f'Chronos {cat} {config["segment_name"]} Forecasts', fontsize=18)
+        plt.title('metrics: EMD:{EMD}, MASE:{MASE}, WQL:{WQL}'.format(**metrics), fontsize = 10, y=1)
+        plt.legend()
+        plt.savefig(f'./chronos_{cat}_{config["segment_name"]}.png')
 
-    ax.plot(plot_dates, np.append(context_by_category['registered']['target'], actuals_by_category['registered']['target']))
-    forecasts_by_category['registered'].plot(ax=ax, show_label=True)
-    fig.autofmt_xdate()
-    plt.title('Registered July Forecasts')
-    plt.legend()
-    plt.savefig('./test_viz_png.png')
 
+def wd(data, forecast_type: str) -> np.ndarray:
+    return_wd = np.ndarray((len(data['label'])))
+    
+    for i in range(len(data['label'])):
+        return_wd += wasserstein_distance(data['label'][i]._get_data(), data[forecast_type][i])
+    return return_wd
 
-def wd(data ,forecast_type: str) -> float:
-    return wasserstein_distance(data['label'], data[forecast_type])
+def swd(data, forecast_type: str) -> np.ndarray:
+    """
+    scaled wasserstein
+    """
+    return_wd = np.ndarray((len(data['label'])))
+    
+    for i in range(len(data['label'])):
+        norm_pred = data['label'][i]._get_data() / data['seasonal_error'][i]
+        norm_actuals = data[forecast_type][i] / data['seasonal_error'][i]
+        return_wd += wasserstein_distance(norm_pred, norm_actuals)
+    return return_wd
+
 
 @dataclass 
 class EMD(BaseMetricDefinition):
@@ -109,28 +134,19 @@ class EMD(BaseMetricDefinition):
 
     forecast_type: str = "0.5"
 
-    def __call__(self, axis: Optional[int] = None) -> DirectMetric:
+    def __call__(self, axis: int) -> DirectMetric:
         return DirectMetric(
-            name=f"EMD_{self.forecast_type}",
-            stat=partial(wd, forecast_type=self.forecast_type),
-            aggregate=Mean(axis=axis), 
+            name=f"EMD[{self.forecast_type}]",
+            stat=partial(swd, forecast_type=self.forecast_type),
+            aggregate=Mean(axis=0), # hard code as kludge
         )
     
 
 
 if __name__ == '__main__':
-    emd = EMD()
-    A = [0,1, 3] 
-    B = [5, 6, 8]
-    Data = {'label': A, 'forecast': B}
-    forecast_type = 'forecast'
-    print(wd(Data, forecast_type))
-    
-    
-    if False:
-        config_file_path = os.environ.get('CHRONOS_EVAL_CONFIG')
-        config = load_config(config_file_path)
-        test_data = load_and_split_dataset(backtest_config=config)
-        forecast = mk_forecasts(test_data)
-        mk_viz([item for item in test_data], forecast, config)
-        print(mk_metrics(test_data, forecast))
+    config_file_path = os.environ.get('CHRONOS_EVAL_CONFIG')
+    config = load_config(config_file_path)
+    test_data = load_and_split_dataset(backtest_config=config)
+    forecast = mk_forecasts(test_data)
+    mk_viz(test_data, forecast, config)
+    print(mk_metrics(test_data, forecast))
