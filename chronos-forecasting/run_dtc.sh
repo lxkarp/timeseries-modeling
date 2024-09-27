6c547171b430f8e138466c17f7481db2237537c2
Alix Karpinski <me@alexkarpinski.com>
2024-09-27 13:02:23 -0400

    chore: cleanup obviously unused files

--- chronos-forecasting/run_dtc.sh
+++
@@ -1,3 +0,0 @@
-CONFIG_FILE=evaluation/configs/zero-shot.yaml 
-OUTPUT_FILE=evaluation/results/chronos-t5-small-zero-shot.csv
-python3 evaluation/evaluate.py $CONFIG_FILE $OUTPUT_FILE --chronos-model-id "amazon/chronos-t5-small"  --batch-size=32 --device=cuda:0 --num-samples 20
